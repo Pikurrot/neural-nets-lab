9ccf6165181a37ace2bba3059350276e13bcf2e6
Pikurrot <pikurrot@gmail.com>
2024-07-28 20:06:48 +0200

    Refactor

--- FFN/model.py
+++ FFN/model.py
@@ -9,11 +9,11 @@ class FFN(torch.nn.Module):
 			hidden_dim: int,
 			output_dim: int
 	):
-		super(FFN, self).__init__()						# (b, input_dim)
-		self.fc1 = nn.Linear(input_dim, hidden_dim)		# (b, hidden_dim)
-		self.fc2 = nn.Linear(hidden_dim, output_dim)	# (b, output_dim)
+		super(FFN, self).__init__()						
+		self.fc1 = nn.Linear(input_dim, hidden_dim)		
+		self.fc2 = nn.Linear(hidden_dim, output_dim)
 
-	def forward(self, x: torch.Tensor):
-		x = F.relu(self.fc1(x))
-		x = self.fc2(x)
+	def forward(self, x: torch.Tensor):	# (b, input_dim)
+		x = F.relu(self.fc1(x))			# (b, hidden_dim)
+		x = self.fc2(x)					# (b, output_dim)
 		return x
